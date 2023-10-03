b6ff88092e4df41dd5f2751bf7814cdfc8488f7d
Ade Widyatama Dian Boernama <adewidyatamadb@gmail.com>
2023-10-03 13:40:09 +0800

    Adding shared student model

---
+++ Shared/SharedStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class SharedStudent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
